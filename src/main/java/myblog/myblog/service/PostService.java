074255f0e10c1830f0e7e6740823cd23a29e7772
sojk401 <sojk401@gmail.com>
2023-04-14 21:56:52 +0900

    Feat: 게시글 등록 기능 추가
    PostService 클래스에 register() 메서드 추가
    
    Related to: #5

--- src/main/java/myblog/myblog/service/PostService.java
+++ src/main/java/myblog/myblog/service/PostService.java
@@ -1,12 +1,16 @@
 package myblog.myblog.service;
 
 import lombok.RequiredArgsConstructor;
+import myblog.myblog.domain.Post;
+import myblog.myblog.dto.PostRequestDTO;
 import myblog.myblog.dto.PostResponseDTO;
 import myblog.myblog.repository.PostRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -19,9 +23,17 @@ public class PostService {
     /**
      * 전체 게시글 조회
      */
-    public List<PostResponseDTO> findAll() {
+    public List<PostResponseDTO> list() {
         return postRepository.findAll().stream()
                 .map(PostResponseDTO::new)
                 .collect(Collectors.toList());
+    }
+
+    /**
+     * 게시글 등록
+     */
+    public PostResponseDTO register(PostRequestDTO postRequestDTO) {
+        Post savedPost = postRepository.save(postRequestDTO.toEntity());
+        return new PostResponseDTO(savedPost);
     }
 }
