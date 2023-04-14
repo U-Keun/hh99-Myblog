270398eb23893ad0774321a6d6dc7dc1fd30c527
sojk401 <sojk401@gmail.com>
2023-04-14 21:57:52 +0900

    Test: 게시글 등록 기능 테스트
    Related to: #5

--- src/test/java/myblog/myblog/service/PostServiceTest.java
+++ src/test/java/myblog/myblog/service/PostServiceTest.java
@@ -2,6 +2,7 @@ package myblog.myblog.service;
 
 import jakarta.transaction.Transactional;
 import myblog.myblog.domain.Post;
+import myblog.myblog.dto.PostRequestDTO;
 import myblog.myblog.repository.PostRepository;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
@@ -16,7 +17,7 @@ class PostServiceTest {
     @Autowired PostRepository postRepository;
 
     @Test
-    @DisplayName("전체 회원 조회")
+    @DisplayName("전체 게시글 조회")
     public void findAll() {
         //given
         long id = 1L;
@@ -24,9 +25,22 @@ class PostServiceTest {
         postRepository.save(post);
 
         //when
-        Long findId = postService.findAll().get(0).getId();
+        Long findId = postService.list().get(0).getId();
 
         //then
         Assertions.assertThat(id).isEqualTo(findId);
+    }
+
+    @Test
+    @DisplayName("게시글 등록")
+    public void register() {
+        //given
+        PostRequestDTO dto = new PostRequestDTO(null, "스프링", "김무무", "스프링 재미있다", "1234");
+
+        //when
+        Long getId = postService.register(dto).getId();
+
+        //then
+        Assertions.assertThat(1L).isEqualTo(getId);
     }
 }
