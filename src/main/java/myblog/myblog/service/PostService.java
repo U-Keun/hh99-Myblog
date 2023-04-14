657cf0a0000ffc7e25277ee759d7943141968f20
sojk401 <sojk401@gmail.com>
2023-04-15 01:18:44 +0900

    Feat: 게시글 수정 기능 추가
    PostService 클래스에 게시글 내용을 수정하는 update() 메서드 추가
    
    Related to: #8

--- src/main/java/myblog/myblog/service/PostService.java
+++ src/main/java/myblog/myblog/service/PostService.java
@@ -10,7 +10,6 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.NoSuchElementException;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -66,5 +65,28 @@ public class PostService {
         postRepository.deleteById(id);
 
         return "게시글 삭제 성공";
+    }
+
+    /**
+     * 게시글 수정
+     */
+    @Transactional
+    public PostResponseDTO update(Long id, PostRequestDTO reqDTO) {
+        Post post = postRepository.findById(id).orElseThrow(
+                () -> new NoSuchElementException("게시글이 존재하지 않습니다.")
+        );
+
+        String savedPassword = post.getPassword();
+        String reqPassword = reqDTO.getPassword();
+
+        //입력한 비밀번호와 저장된 비밀번호가 같으면 게시글 수정
+        if (!savedPassword.equals(reqPassword)) {
+            throw new NoSuchElementException("게시글 비밀번호가 다릅니다.");
+        }
+
+        //영속성 컨텍스트로 관리되므로 DB에 변경내용이 반영됨
+        //게시글 변경 내용 적용
+        post.update(reqDTO);
+        return new PostResponseDTO(post);
     }
 }
