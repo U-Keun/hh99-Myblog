3a4537b1fd2b48b96793e7d8bfb02b7f07c20d9e
sojk401 <sojk401@gmail.com>
2023-04-14 21:59:12 +0900

    Feat: 게시글 등록 Post 요청 기능 추가
    PostController에서 게시글 등록 Post 요청 기능을 수행하는 register() 메서드 추가
    
    Resolves: #5

--- src/main/java/myblog/myblog/controller/PostController.java
+++ src/main/java/myblog/myblog/controller/PostController.java
@@ -1,25 +1,31 @@
 package myblog.myblog.controller;
 
 import lombok.RequiredArgsConstructor;
+import myblog.myblog.dto.PostRequestDTO;
 import myblog.myblog.dto.PostResponseDTO;
 import myblog.myblog.service.PostService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/post")
+@RequestMapping("post")
 @RequiredArgsConstructor
 public class PostController {
+
     private final PostService postService;
 
     @GetMapping()
     @ResponseBody
     public List<PostResponseDTO> list() {
-        List<PostResponseDTO> posts = postService.findAll();
+        List<PostResponseDTO> posts = postService.list();
         return posts;
+    }
+
+    @PostMapping()
+    @ResponseBody
+    public PostResponseDTO register(PostRequestDTO dto) {
+        PostResponseDTO savedPost = postService.register(dto);
+        return savedPost;
     }
 }
