5dc9f173216e9853b7a821b07aa972589d6c68fe
sojk401 <sojk401@gmail.com>
2023-04-18 01:38:28 +0900

    Refactor: 메서드의 반환 타입 변경 및 유효성 체크 추가
    비밀번호 일치 여부 확인 메서드 생성
    Id 중복 여부 확인 확인 메서드 생성

--- src/main/java/myblog/myblog/service/MemberService.java
+++ src/main/java/myblog/myblog/service/MemberService.java
@@ -9,6 +9,8 @@ import myblog.myblog.dto.MessageDTO;
 import myblog.myblog.dto.SignupRequestDTO;
 import myblog.myblog.jwt.JwtUtil;
 import myblog.myblog.repository.MemberRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -20,37 +22,55 @@ public class MemberService {
     private final MemberRepository memberRepository;
     private final JwtUtil jwtUtil;
 
-    public MessageDTO signup(SignupRequestDTO requestDto) {
+    public ResponseEntity signup(SignupRequestDTO requestDto) {
         String username = requestDto.getUsername();
         String password = requestDto.getPassword();
 
         // 회원 중복 확인
-        Optional<Member> found = memberRepository.findByUsername(username);
-        if (found.isPresent()) {
-            throw new IllegalArgumentException("중복된 사용자가 존재합니다.");
-        }
+        validateDuplicateMember(username);
 
         Member user = new Member(username, password);
         memberRepository.save(user);
-        return new MessageDTO(StatusCode.OK, "signup success", null);
+        MessageDTO messageDTO = new MessageDTO(StatusCode.OK, "signup success", null);
+        return new ResponseEntity(messageDTO, HttpStatus.OK);
     }
 
-    public MessageDTO login(LoginRequestDTO requestDTO, HttpServletResponse response) {
+    public ResponseEntity login(LoginRequestDTO requestDTO, HttpServletResponse response) {
         String username = requestDTO.getUsername();
         String password = requestDTO.getPassword();
 
         //회원 체크
-        Member member = memberRepository.findByUsername(username).orElseThrow(
-                () -> new IllegalArgumentException("등록된 사용자가 없습니다.")
-        );
+        Member member = chekcMember(username);
 
         //비밀번호 체크
+        checkPassword(password, member);
+
+        //응답 헤더에 토큰 추가
+        response.addHeader(JwtUtil.AUTHORIZATION_HEADER, jwtUtil.createToken(member.getUsername()));
+        MessageDTO messageDTO = new MessageDTO(StatusCode.OK, "login success", null);
+        return new ResponseEntity(messageDTO, HttpStatus.OK);
+    }
+
+    //회원 여부 체크
+    private Member chekcMember(String username) {
+        Member member = memberRepository.findByUsername(username).orElseThrow(
+                () -> new IllegalArgumentException("등록된 회원이 없습니다.")
+        );
+        return member;
+    }
+
+    //Id 중복 체크
+    private void validateDuplicateMember(String username) {
+        Optional<Member> found = memberRepository.findByUsername(username);
+        if (found.isPresent()) {
+            throw new IllegalArgumentException("중복된 Id가 존재합니다.");
+        }
+    }
+
+    //비밀번호 일치 여부 체크
+    private void checkPassword(String password, Member member) {
         if(!member.getPassword().equals(password)) {
             throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
         }
-
-        //응답 헤더에 토큰 추가
-        response.addHeader(JwtUtil.AUTHORIZATION_HEADER, jwtUtil.createToken(member.getUserName()));
-        return new MessageDTO(StatusCode.OK, "login success", null);
     }
 }
