1bbcb30909857155d5079234938dc7721011f004
sojk401 <sojk401@gmail.com>
2023-04-18 01:39:03 +0900

    Refactor: PostController 메서드의 반환 타입 변경

--- src/main/java/myblog/myblog/controller/PostController.java
+++ src/main/java/myblog/myblog/controller/PostController.java
@@ -2,13 +2,9 @@ package myblog.myblog.controller;
 
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
-import myblog.myblog.dto.MessageDTO;
-import myblog.myblog.domain.StatusCode;
 import myblog.myblog.dto.PostRequestDTO;
 import myblog.myblog.dto.PostResponseDTO;
 import myblog.myblog.service.PostService;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -22,43 +18,32 @@ public class PostController {
 
     //전체 게시글 조회
     @GetMapping()
-    @ResponseBody
-    public ResponseEntity list() {
+    public List<PostResponseDTO> list() {
         List<PostResponseDTO> posts = postService.list();
-        MessageDTO messageDTO = new MessageDTO(StatusCode.OK, "list success", posts);
-        return new ResponseEntity(messageDTO, HttpStatus.OK);
+        return posts;
     }
-
     //게시글 등록
     @PostMapping()
-    @ResponseBody
-    public ResponseEntity register(@RequestBody PostRequestDTO dto, HttpServletRequest request) {
-        MessageDTO messageDTO = postService.savePost(dto, request);
-        String code = messageDTO.getStatus().getCode();
-        return new ResponseEntity(messageDTO, HttpStatus.OK);
+    public PostResponseDTO  register(@RequestBody PostRequestDTO dto, HttpServletRequest request) {
+        PostResponseDTO savedPost = postService.savePost(dto, request);
+        return savedPost;
     }
 
     //특정 게시글 조회
     @GetMapping("/{id}")
-    @ResponseBody
-    public ResponseEntity findPost(@PathVariable Long id) {
-        MessageDTO messageDTO = postService.findPostById(id);
-        return new ResponseEntity(messageDTO, HttpStatus.OK);
+    public PostResponseDTO  findPost(@PathVariable Long id) {
+        return postService.findPostById(id);
     }
 
     //게시글 삭제
     @DeleteMapping("/delete/{id}")
-    @ResponseBody
-    public ResponseEntity delete(@PathVariable Long id, HttpServletRequest request) {
-        MessageDTO messageDTO = postService.deletePost(id, request);
-        return new ResponseEntity(messageDTO, HttpStatus.OK);
+    public PostResponseDTO  delete(@PathVariable Long id, HttpServletRequest request) {
+        return postService.deletePost(id, request);
     }
 
     //게시글 수정
     @PutMapping("/update/{id}")
-    @ResponseBody
-    public ResponseEntity update(@PathVariable Long id, @RequestBody PostRequestDTO reqDTO, HttpServletRequest request) {
-        MessageDTO messageDTO = postService.updatePost(id, reqDTO, request);
-        return new ResponseEntity(messageDTO, HttpStatus.OK);
+    public PostResponseDTO update(@PathVariable Long id, @RequestBody PostRequestDTO reqDTO, HttpServletRequest request) {
+        return postService.updatePost(id, reqDTO, request);
     }
 }
