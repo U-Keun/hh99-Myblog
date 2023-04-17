38b695d8fe38a6d24f5ed46502f3f11c7b3ce970
sojk401 <sojk401@gmail.com>
2023-04-17 21:47:25 +0900

    Feat: MemberController ResponseEntity를 사용한 응답 처리

--- src/main/java/myblog/myblog/controller/MemberController.java
+++ src/main/java/myblog/myblog/controller/MemberController.java
@@ -2,30 +2,29 @@ package myblog.myblog.controller;
 
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
-import myblog.myblog.dto.LoginRequestDto;
-import myblog.myblog.dto.SignupRequestDto;
-import myblog.myblog.repository.MemberRepository;
+import myblog.myblog.dto.LoginRequestDTO;
+import myblog.myblog.dto.SignupRequestDTO;
+import myblog.myblog.service.MemberService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("user")
+@RequestMapping("member")
 @RequiredArgsConstructor
 public class MemberController {
 
     private final MemberService memberService;
 
     @PostMapping("/signup")
-    public ResponseEntity signup(SignupRequestDto signupRequestDto) {
-        memberService.signup(signupRequestDto);
-        return new ResponseEntity(HttpStatus.OK);
+    public ResponseEntity signup(@RequestBody SignupRequestDTO signupRequestDto) {
+        String result = memberService.signup(signupRequestDto);
+        return new ResponseEntity(result, HttpStatus.OK);
     }
 
-    @ResponseBody
     @PostMapping("/login")
-    public ResponseEntity login(@RequestBody LoginRequestDto loginRequestDto, HttpServletResponse response) {
-        memberService.login(loginRequestDto, response);
-        return new ResponseEntity(HttpStatus.OK);
+    public ResponseEntity login(@RequestBody LoginRequestDTO loginRequestDto, HttpServletResponse response) {
+        String result = memberService.login(loginRequestDto, response);
+        return new ResponseEntity(result, HttpStatus.OK);
     }
 }
