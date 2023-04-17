f87d522eb42cbd46784bdf3ff84634ae61f43e29
sojk401 <sojk401@gmail.com>
2023-04-18 02:10:04 +0900

    Refactor: 비밀번호 유효성 검사 정규식 변경
    최소8, 최대15 글자 이하이며 알파벳 대소문자, 숫자, 특수문자로 구성되어야 함

--- src/main/java/myblog/myblog/dto/SignupRequestDTO.java
+++ src/main/java/myblog/myblog/dto/SignupRequestDTO.java
@@ -1,5 +1,6 @@
 package myblog.myblog.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
@@ -8,10 +9,12 @@ import lombok.Data;
 public class SignupRequestDTO {
 
     @Size(min = 4, max = 10)
+    @NotBlank(message = "아이디는 필수 입력 값입니다.")
     @Pattern(regexp = "^[a-z0-9]*$" , message = "아이디 형식이 올바르지 않습니다.")
     private String username;
 
     @Size(min = 8, max = 15)
-    @Pattern(regexp = "^[a-zA-Z0-9]*$" , message = "비밀번호 형식이 올바르지 않습니다.")
+    @NotBlank(message = "비밀번호는 필수 입력 값입니다.")
+    @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,15}", message = "비밀번호는 8~15자 영문 대 소문자, 숫자, 특수문자를 사용하세요.")
     private String password;
 }
