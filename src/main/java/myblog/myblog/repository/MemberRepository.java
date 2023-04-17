d3e0dcb48640ebc1f75918fe0fe54373947d8a16
sojk401 <sojk401@gmail.com>
2023-04-17 21:10:56 +0900

    Feat: MemberRepository username으로 Member 객체 찾는 기능 추가

--- src/main/java/myblog/myblog/repository/MemberRepository.java
+++ src/main/java/myblog/myblog/repository/MemberRepository.java
@@ -3,5 +3,8 @@ package myblog.myblog.repository;
 import myblog.myblog.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByUsername(String username);
 }
