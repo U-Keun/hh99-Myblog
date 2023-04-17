4628cf221e3bbd25f4d41190a138eac4353c51d0
sojk401 <sojk401@gmail.com>
2023-04-17 21:13:49 +0900

    Feat: MemberService 회원 가입 및 로그인 기능 추가
    회원가입시, 아이디 중복 검사 후 회원 등록
    로그인시, 아이디로 회원 존재 여부 판단 및 비밀번호 일치 여부 판단
    
    Related to: #9

---
+++ src/main/java/myblog/myblog/service/MemberService.java
@@ -0,0 +1,54 @@
+package myblog.myblog.service;
+
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import myblog.myblog.domain.Member;
+import myblog.myblog.dto.LoginRequestDTO;
+import myblog.myblog.dto.SignupRequestDTO;
+import myblog.myblog.jwt.JwtUtil;
+import myblog.myblog.repository.MemberRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+    private final JwtUtil jwtUtil;
+
+    public String signup(SignupRequestDTO requestDto) {
+        String username = requestDto.getUsername();
+        String password = requestDto.getPassword();
+
+        // 회원 중복 확인
+        Optional<Member> found = memberRepository.findByUsername(username);
+        if (found.isPresent()) {
+            throw new IllegalArgumentException("중복된 사용자가 존재합니다.");
+        }
+
+        Member user = new Member(username, password);
+        memberRepository.save(user);
+        return "signup success";
+    }
+
+    public String login(LoginRequestDTO requestDTO, HttpServletResponse response) {
+        String username = requestDTO.getUsername();
+        String password = requestDTO.getPassword();
+
+        //회원 체크
+        Member member = memberRepository.findByUsername(username).orElseThrow(
+                () -> new IllegalArgumentException("등록된 사용자가 없습니다.")
+        );
+
+        //비밀번호 체크
+        if(!member.getPassword().equals(password)) {
+            throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
+        }
+
+        //응답 헤더에 토큰 추가
+        response.addHeader(JwtUtil.AUTHORIZATION_HEADER, jwtUtil.createToken(member.getUserName()));
+        return "login success";
+    }
+}
