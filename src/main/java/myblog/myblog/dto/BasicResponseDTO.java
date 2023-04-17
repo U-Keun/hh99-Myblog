a45b316fccc7e7db346edf935b5ffdf521fe6ab5
sojk401 <sojk401@gmail.com>
2023-04-18 01:50:36 +0900

    Rename: MessageDTO를 BasicResponseDTO로 이름 변경

--- src/main/java/myblog/myblog/dto/BasicResponseDTO.java
+++ src/main/java/myblog/myblog/dto/BasicResponseDTO.java
@@ -4,12 +4,12 @@ import lombok.Data;
 import myblog.myblog.domain.StatusCode;
 
 @Data
-public class MessageDTO {
+public class BasicResponseDTO {
     private StatusCode status;
     private String message;
     private Object data;
 
-    public MessageDTO(StatusCode status, String message, Object data) {
+    public BasicResponseDTO(StatusCode status, String message, Object data) {
         this.status = status;
         this.message = message;
         this.data = data;
