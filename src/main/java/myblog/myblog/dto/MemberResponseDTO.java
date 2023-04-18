11ea23f420a337b93e0e1d4edd0715f2b6cfdcdd
sojk401 <sojk401@gmail.com>
2023-04-19 03:25:55 +0900

    Feat: MemberResponseDTO 생성

---
+++ src/main/java/myblog/myblog/dto/MemberResponseDTO.java
@@ -0,0 +1,21 @@
+package myblog.myblog.dto;
+
+import lombok.Getter;
+import lombok.ToString;
+import myblog.myblog.domain.Member;
+import myblog.myblog.domain.UserRole;
+
+@Getter
+@ToString
+public class MemberResponseDTO {
+
+    private Long id;
+    private String username;
+    private UserRole role;
+
+    public MemberResponseDTO(Member member) {
+        this.id = member.getId();
+        this.username = member.getUsername();
+        this.role = member.getRole();
+    }
+}
