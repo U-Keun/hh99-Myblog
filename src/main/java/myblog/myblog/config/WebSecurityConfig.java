7f2805bc3476acc4bf06363f2f443aa46680e8c3
sojk401 <sojk401@gmail.com>
2023-04-20 17:56:43 +0900

    Refactor: 스프링 시큐리티 관련 코드 수정 및 추가

--- src/main/java/myblog/myblog/config/WebSecurityConfig.java
+++ src/main/java/myblog/myblog/config/WebSecurityConfig.java
@@ -1,22 +1,63 @@
 package myblog.myblog.config;
 
+import lombok.RequiredArgsConstructor;
 import myblog.myblog.jwt.JwtAuthenticationFilter;
-import org.springframework.beans.factory.annotation.Autowired;
+import myblog.myblog.jwt.TokenProvider;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
+@EnableGlobalMethodSecurity(securedEnabled = true) // @Secured 어노테이션 활성화
 public class WebSecurityConfig {
-    @Autowired
-    JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+    private final TokenProvider tokenProvider;
+
     @Bean
-    protected SecurityFilterChain configure(HttpSecurity httpSecurity) throws Exception {
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+/*    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        // h2-console 사용 및 resources 접근 허용 설정
+        return (web) -> web.ignoring()
+                .requestMatchers(PathRequest.toH2Console())
+                .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+    }*/
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+
+        // 기본 설정인 Session 방식은 사용하지 않고 JWT 방식을 사용하기 위한 설정
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        // 해당 경로로 들어오는 요청은 인증 처리 무시
+        http.authorizeRequests().requestMatchers("/member/**").permitAll()
+                .requestMatchers(PathRequest.toH2Console()).permitAll() //H2-console 허용
+                .requestMatchers("/board/**").permitAll()
+                .anyRequest().authenticated()
+                // JWT 인증/인가를 사용하기 위한 설정
+                .and().addFilterBefore(new JwtAuthenticationFilter(tokenProvider), UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+
+/*    @Bean
+    protected SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
                 // cors 정책 (현재는 Application에서 작업을 해뒀으므로 기본 설정 사용)
                 .cors().and()
@@ -27,12 +68,12 @@ public class WebSecurityConfig {
                 // 세션 기반 인증 (현재는 Session 기반 인증을 사용하지 않기 때문에 상태를 없앰)
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
                 // '/', '/api/member' 모듈에 대해서는 모두 허용 (인증을 하지 않고 사용 가능하게 함)
-                .authorizeHttpRequests().requestMatchers("/","/member/**","/board/**").permitAll()
+                .authorizeHttpRequests().requestMatchers("/member/**", "/board/**").permitAll()
                 // 나머지 Request에 대해서는 모두 인증된 사용자만 사용가능하게 함
                 .anyRequest().authenticated();
 
         httpSecurity.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
         return httpSecurity.build();
-    }
+    }*/
 }
--- src/main/java/myblog/myblog/jwt/JwtAuthenticationFilter.java
+++ src/main/java/myblog/myblog/jwt/JwtAuthenticationFilter.java
@@ -1,18 +1,17 @@
 package myblog.myblog.jwt;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import myblog.myblog.dto.SecurityExceptionDto;
+import org.springframework.security.core.Authentication;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import myblog.myblog.security.TokenProvider;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.AbstractAuthenticationToken;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -21,21 +20,66 @@ import java.io.IOException;
 
 @Slf4j
 @Component
+@RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
     // request가 들어왔을 때 Request Header의 Authorization 필드의 Bearer Token을 가져옴
     // 가져온 토큰을 검증하고 검증 결과를 SecurityContext에 추가
-    @Autowired
-    private TokenProvider tokenProvider;
+
+    private final TokenProvider tokenProvider;
 
     @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        //헤더의 토큰 가져오기
+        String token = tokenProvider.resolveToken(request, "Access");
+
+        //토큰 존재 여부 판단
+        if(token != null) {
+            if(!tokenProvider.validateToken(token)){
+                jwtExceptionHandler(response, "Token Error", HttpStatus.UNAUTHORIZED.value());
+                return;
+            }
+            String username = tokenProvider.getUserInfoFromToken(token);
+            setAuthentication(username);
+        }
+        filterChain.doFilter(request,response);
+    }
+
+    public void setAuthentication(String username) {
+        SecurityContext context = SecurityContextHolder.createEmptyContext();
+        Authentication authentication = tokenProvider.createAuthentication(username);
+        context.setAuthentication(authentication);
+
+        SecurityContextHolder.setContext(context);
+    }
+
+    public void jwtExceptionHandler(HttpServletResponse response, String msg, int statusCode) {
+        response.setStatus(statusCode);
+        response.setContentType("application/json");
+        try {
+            String json = new ObjectMapper().writeValueAsString(new SecurityExceptionDto(statusCode, msg));
+            response.getWriter().write(json);
+        } catch (Exception e) {
+            log.error(e.getMessage());
+        }
+    }
+
+    private String parseBearerToken(HttpServletRequest request, String token) {
+        String bearerToken = request.getHeader("Authorization");
+        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer "))
+            return bearerToken.substring(7);
+        return null;
+    }
+
+/*    @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
         try {
             String token = parseBearerToken(request);
 
             if (token != null && !token.equalsIgnoreCase("null")) {
-                // 토큰 검증해서 payload의 userEmail 가져옴
-                String username = tokenProvider.validate(token); //수정
+                // 토큰 검증해서 payload의 username 가져옴
+                String username = tokenProvider.getUsernameFromToken(token); //수정
                 // SecurityContext에 추가할 객체
                 AbstractAuthenticationToken authentication =
                         new UsernamePasswordAuthenticationToken(username, null, AuthorityUtils.NO_AUTHORITIES);
@@ -56,10 +100,5 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     }
 
     // Request Header의 Authorization 필드의 Bearer Token을 가져오는 메서드
-    private String parseBearerToken(HttpServletRequest request) {
-        String bearerToken = request.getHeader("Authorization");
-        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer "))
-            return bearerToken.substring(7);
-        return null;
-    }
+    */
 }
