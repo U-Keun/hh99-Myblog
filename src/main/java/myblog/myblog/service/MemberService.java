aae072de9187e1d03edeb6693588ea732bf18882
sojk401 <sojk401@gmail.com>
2023-04-20 18:03:54 +0900

    Chore: 주석 추가

--- src/main/java/myblog/myblog/service/MemberService.java
+++ src/main/java/myblog/myblog/service/MemberService.java
@@ -3,30 +3,37 @@ package myblog.myblog.service;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import myblog.myblog.domain.Member;
-import myblog.myblog.domain.UserRole;
+import myblog.myblog.domain.UserRoleEnum;
 import myblog.myblog.dto.member.LoginRequestDTO;
 import myblog.myblog.dto.BasicResponseDTO;
 import myblog.myblog.dto.member.MemberResponseDTO;
 import myblog.myblog.dto.member.SignupRequestDTO;
 import myblog.myblog.exception.MemberException;
 import myblog.myblog.repository.MemberRepository;
-import myblog.myblog.security.TokenProvider;
+import myblog.myblog.jwt.TokenProvider;
 import myblog.myblog.util.ExceptionMessage;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+
 public class MemberService {
 
     private final MemberRepository memberRepository;
     private final TokenProvider tokenProvider;
+    private final PasswordEncoder passwordEncoder;
 
+    /**
+     * 회원가입
+     */
     public ResponseEntity signup(SignupRequestDTO requestDto) {
         String username = requestDto.getUsername();
-        String password = requestDto.getPassword();
-        UserRole role = requestDto.getRole();
+        String password = passwordEncoder.encode(requestDto.getPassword());
+        UserRoleEnum role = requestDto.getRole();
 
         // 회원 중복 확인
         validateDuplicateMember(username);
@@ -37,6 +44,10 @@ public class MemberService {
         return new ResponseEntity(basicResponseDTO, HttpStatus.OK);
     }
 
+    /**
+     * 로그인
+     */
+    @Transactional(readOnly = true)
     public ResponseEntity login(LoginRequestDTO requestDTO, HttpServletResponse response) {
         String username = requestDTO.getUsername();
         String password = requestDTO.getPassword();
@@ -44,13 +55,11 @@ public class MemberService {
         //회원 체크
         Member member = validateMember(username);
 
-        UserRole role = member.getRole();
-
         //비밀번호 체크
         validatePassword(password, member);
 
         //응답 헤더에 토큰 추가
-        response.addHeader(tokenProvider.AUTHORIZATION_HEADER, tokenProvider.create(member.getUsername(), role));
+        response.addHeader(tokenProvider.ACCESS_KEY, tokenProvider.create(member.getUsername(), member.getRole(), "Access"));
         BasicResponseDTO<MemberResponseDTO> basicResponseDTO = BasicResponseDTO.setSuccess("login success", new MemberResponseDTO(member));
         return new ResponseEntity(basicResponseDTO, HttpStatus.OK);
     }
