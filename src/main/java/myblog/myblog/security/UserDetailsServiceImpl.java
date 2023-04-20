0d2d75b31b7f01fece0b6a69778c3450c92d095b
sojk401 <sojk401@gmail.com>
2023-04-20 17:58:09 +0900

    Feat: UserDetailService와 UserDetailsImpl 생성

---
+++ src/main/java/myblog/myblog/security/UserDetailsServiceImpl.java
@@ -0,0 +1,26 @@
+package myblog.myblog.security;
+
+import lombok.RequiredArgsConstructor;
+import myblog.myblog.domain.Member;
+import myblog.myblog.repository.MemberRepository;
+import myblog.myblog.security.UserDetailsImpl;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final MemberRepository memberRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Member member = memberRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("사용자를 찾을 수 없습니다."));
+
+        return new UserDetailsImpl(member, member.getUsername());
+    }
+
+}
