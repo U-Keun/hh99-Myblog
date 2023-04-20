2249bced6ef215f7f38b065fb0d3676f6923fe19
sojk401 <sojk401@gmail.com>
2023-04-20 11:28:32 +0900

    Feat: MemberException, CommentException, PostException 사용자 정의 예외 클래스 생성

---
+++ src/main/java/myblog/myblog/exception/PostException.java
@@ -0,0 +1,19 @@
+package myblog.myblog.exception;
+
+public class PostException extends RuntimeException{
+    public PostException() {
+        super();
+    }
+
+    public PostException(String message) {
+        super(message);
+    }
+
+    public PostException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public PostException(Throwable cause) {
+        super(cause);
+    }
+}
